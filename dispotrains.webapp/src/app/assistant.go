1c6503831ba5365446214b5d89975a30dc5fd8dc
Etienne J. Membrives <etienne@membrives.fr>
2017-12-03 16:45:27 +0100

    Assistant response

--- dispotrains.webapp/src/app/assistant.go
+++ dispotrains.webapp/src/app/assistant.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -10,6 +11,32 @@ import (
 
 	"gopkg.in/mgo.v2/bson"
 )
+
+type webhookResponse struct {
+	FulfillmentText string `json:"fulfillmentText,omitempty"`
+}
+
+func brokenElevators(elevators []*storage.Elevator) []*storage.Elevator {
+	broken := make([]*storage.Elevator, 0)
+	for _, e := range elevators {
+		state := e.GetLastStatus().State
+		if state != "Disponible" && state != "Information non disponible" {
+			broken = append(broken, e)
+		}
+	}
+	return broken
+}
+
+func noInfoElevators(elevators []*storage.Elevator) []*storage.Elevator {
+	noInfo := make([]*storage.Elevator, 0)
+	for _, e := range elevators {
+		state := e.GetLastStatus().State
+		if state == "Information non disponible" {
+			noInfo = append(noInfo, e)
+		}
+	}
+	return noInfo
+}
 
 func FulfillmentHandler(w http.ResponseWriter, req *http.Request) {
 	c := session.DB("dispotrains").C("stations")
@@ -45,5 +72,34 @@ func FulfillmentHandler(w http.ResponseWriter, req *http.Request) {
 		log.Println(err)
 		return
 	}
-	json.NewEncoder(w).Encode(&station)
+	response := webhookResponse{}
+	response.FulfillmentText = fmt.Sprintf("Au %s, la gare de %s", station.LastUpdate.Format("02/01 à 15 heures 04"), station.DisplayName)
+	if station.Available() {
+		response.FulfillmentText = response.FulfillmentText + "n'a aucun ascenseur en panne."
+	} else {
+		broken := brokenElevators(station.Elevators)
+		noInfo := noInfoElevators(station.Elevators)
+		var summary string
+		var descriptions string
+		if len(broken) == 0 && len(noInfo) == 0 {
+			summary = fmt.Sprintf(" a %d sur %d ascenseurs sans relevé.", len(noInfo), len(station.Elevators))
+		} else if len(broken) != 0 {
+			summary = fmt.Sprintf(" a %d sur %d ascenseurs en panne et %d sans relevé. ", len(broken), len(station.Elevators), len(noInfo))
+			descriptions = "Les ascenseurs en panne sont: "
+			for i, e := range broken {
+				if i != 0 {
+					descriptions += " ; "
+				}
+				descriptions += fmt.Sprintf("%s", e.Situation)
+				if len(e.Direction) != 0 {
+					descriptions += ", direction " + e.Direction
+				}
+				if e.Status.Forecast != nil {
+					descriptions += ", jusqu'au " + e.Status.Forecast.Format("02/01/2006")
+				}
+			}
+		}
+		response.FulfillmentText = response.FulfillmentText + summary + descriptions
+	}
+	json.NewEncoder(w).Encode(&response)
 }
