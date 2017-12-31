baf7d8cd20553e39a4cb99b126bc7064e5707684
Etienne J. Membrives <etienne@membrives.fr>
2017-12-31 12:51:16 +0100

    Average, not median

--- dispotrains.webapp/src/app/statistics.go
+++ dispotrains.webapp/src/app/statistics.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
-	"sort"
 	"time"
 
 	"github.com/emembrives/dispotrains/dispotrains.webapp/src/storage"
@@ -44,41 +43,31 @@ func ElevatorHandler(w http.ResponseWriter, req *http.Request) {
 	if err := cStatistics.Find(bson.M{"elevator": elevatorID, "end": bson.M{"$gt": datetime}}).Sort("-begin").All(&stats); err != nil {
 		log.Println(err)
 	}
-	availableStats := make([]storage.ElevatorState, 0)
-	brokenStats := make([]storage.ElevatorState, 0)
-	var broken, total time.Duration
+	var availableTime, brokenTime, totalTime time.Duration
+	var availablePeriods, brokenPeriods int
 	for _, stat := range stats {
 		if stat.State == "Disponible" {
-			availableStats = append(availableStats, stat)
+			availableTime += stat.End.Sub(stat.Begin)
+			availablePeriods++
 		} else {
-			brokenStats = append(brokenStats, stat)
-			broken += stat.End.Sub(stat.Begin)
+			brokenTime += stat.End.Sub(stat.Begin)
+			brokenPeriods++
 		}
-		total += stat.End.Sub(stat.Begin)
+		totalTime += stat.End.Sub(stat.Begin)
 	}
-	sort.Slice(availableStats, func(i, j int) bool {
-		iLen := availableStats[i].End.Sub(availableStats[i].Begin)
-		jLen := availableStats[j].End.Sub(availableStats[j].Begin)
-		return iLen < jLen
-	})
-	sort.Slice(brokenStats, func(i, j int) bool {
-		iLen := brokenStats[i].End.Sub(brokenStats[i].Begin)
-		jLen := brokenStats[j].End.Sub(brokenStats[j].Begin)
-		return iLen < jLen
-	})
 	out := outStats{}
-	if len(availableStats) != 0 {
-		out.Mtbf = availableStats[len(availableStats)/2].End.Sub(availableStats[len(availableStats)/2].Begin)
+	if availablePeriods != 0 {
+		out.Mtbf = availableTime / time.Duration(availablePeriods)
 	} else {
 		out.Mtbf = 0
 	}
-	if len(brokenStats) != 0 {
-		out.Mtbr = brokenStats[len(brokenStats)/2].End.Sub(brokenStats[len(brokenStats)/2].Begin)
+	if brokenPeriods != 0 {
+		out.Mtbr = brokenTime / time.Duration(brokenPeriods)
 	} else {
 		out.Mtbr = 0
 	}
-	out.Broken = broken
-	out.Total = total
+	out.Broken = brokenTime
+	out.Total = totalTime
 	out.States = stats[0:min(len(stats), 30)]
 	if err := json.NewEncoder(w).Encode(&out); err != nil {
 		log.Println(err)
