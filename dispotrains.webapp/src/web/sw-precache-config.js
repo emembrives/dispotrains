d8d5b4d467e6e9d8871af25ca59ce1e5c5754abe
Etienne J. Membrives <etienne@membrives.fr>
2018-03-22 07:47:08 +0100

    Fix typo, offline

--- dispotrains.webapp/src/web/sw-precache-config.js
+++ dispotrains.webapp/src/web/sw-precache-config.js
@@ -6,15 +6,7 @@ module.exports = {
       [ 'dist/index.html', 'dist/**.js', 'dist/**.css', 'dist/**.map' ],
   maximumFileSizeToCacheInBytes : 5242880,
   runtimeCaching : [ {
-    urlPattern : /^https:\/\/dispotrains\.membrives\.fr\/app\/.*/,
-    handler : 'networkFirst'
-  },
-  {
-    urlPattern : /^https:\/\/dispotrains\.membrives\.fr\/static\/data\/.*/,
-    handler : 'networkOnly'
-  },
-  {
-    urlPattern : /^https:\/\/dispotrains\.membrives\.fr\/assets\/.*/,
+    urlPattern : "/app/:command/",
     handler : 'networkFirst'
   }]
 };
